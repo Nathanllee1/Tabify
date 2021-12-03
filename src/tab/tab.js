3ba4c8e2626e2a86e1c022230fedbe0c97df5ec4
nathanllee1 <nathan.l.lee123@gmail.com>
2021-12-03 15:21:42 -0800

    Updated endpoint to fetch from our scraper

--- src/tab/tab.js
+++ src/tab/tab.js
@@ -58,8 +58,8 @@ function Tab(props) {
     async function getTab() {
 
       if (track) {
-        console.log("Fetching ", `https://tabify-scraper.herokuapp.com/gettab?song_name=${encodeURIComponent(track.name)}&artist_name=${encodeURIComponent(track.artists[0].name)}`)
-        const response = await fetch(`https://tabify-scraper.herokuapp.com/gettab?song_name=${encodeURIComponent(track.name)}&artist_name=${encodeURIComponent(track.artists[0].name)}`);
+        console.log("Fetching ", `/gettab?song_name=${encodeURIComponent(track.name)}&artist_name=${encodeURIComponent(track.artists[0].name)}`)
+        const response = await fetch(`/gettab?song_name=${encodeURIComponent(track.name)}&artist_name=${encodeURIComponent(track.artists[0].name)}`);
         const json = await response.json();
   
         cleanHTML(json.TAB);
