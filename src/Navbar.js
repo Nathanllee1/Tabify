9653470fac6e51edbfbdd8af542ca33f2f6c7c37
esauerbo <emma.m.sauerborn@gmail.com>
2021-12-03 16:39:10 -0800

    styled switch and added svg

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,43 +1,72 @@
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import { makeStyles } from "@material-ui/core/styles";
+import { Row, Col } from "react-bootstrap";
 import { styled } from "@mui/system";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
+import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
 import Switch from "@mui/material/Switch";
+import Icon from "./Icon";
+
+const THEME = createTheme({
+  typography: {
+    fontFamily: ` source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace`,
+  },
+});
 
 const StyledNav = styled(AppBar)`
-  background-color: rgba(0, 0, 0, 0);
+  background-color: rgba(255,255,255, 0.9);
   box-shadow: none;
   color: black;
 `;
 
+const StyledSwitch = styled(Switch)`
+  color:"#1db954";
+`
+
+const StyledForm = styled(FormGroup)`
+  position: absolute;
+  top: 10px;
+  right: 60px;
+`;
+
 export default function ButtonAppBar(props) {
   const { musicConnected, autoScroll, setAutoScroll } = props;
-  
+
   function toggleAutoScroll() {
     setAutoScroll(!autoScroll);
   }
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <StyledNav position="fixed">
+    <Row>
+      <StyledNav>
         <Toolbar>
+        <Col>
           <h1 className="app_logo">Tabify</h1>
-          <img src="tabify_logo.png" />
+          </Col>
+          <Col>
+          <Icon />
+          </Col>
           {musicConnected && (
-            <FormGroup>
-              <FormControlLabel
-                onChange={toggleAutoScroll}
-                control={<Switch />}
-                checked={autoScroll}
-                label="Autoscroll"
-              />
-            </FormGroup>
+            <Col className="float-right">
+            <StyledForm>
+              <MuiThemeProvider theme={THEME}>
+                <FormControlLabel
+                  onChange={toggleAutoScroll}
+                  control={<Switch color='success'  />}
+                  checked={autoScroll}
+                  label=""
+                />
+                <div>Autoscroll</div>
+              </MuiThemeProvider>
+            </StyledForm>
+            </Col>
           )}
         </Toolbar>
       </StyledNav>
+      </Row>
     </Box>
   );
 }
